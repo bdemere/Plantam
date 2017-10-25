e993cb6529b8ecabae5d0d560722962b38583b47
Anh Nguyen <anhnguyen1129@gmail.com>
2017-10-24 22:51:04 -0400

    Anh Brian weeklyview

--- code/CalendarView/app/src/main/java/com/example/mynameisyou/calendarview/MainActivity.java
+++ code/CalendarView/app/src/main/java/com/example/mynameisyou/calendarview/MainActivity.java
@@ -12,17 +12,28 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView theDate;
     private Button btnGoCalendar;
+    private Button btnGoWeek;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         theDate = (TextView) findViewById(R.id.date);
-        btnGoCalendar = (Button) findViewById(R.id.btnGoCalendar);
 
+        btnGoCalendar = (Button) findViewById(R.id.btnGoCalendar);
         btnGoCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnGoWeek = (Button) findViewById(R.id.btnGoWeek);
+        btnGoWeek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //setContentView(R.layout.weekly_layout);
+                Intent intent = new Intent(MainActivity.this, WeekActivity.class);
                 startActivity(intent);
             }
         });
---
+++ code/CalendarView/app/src/main/java/com/example/mynameisyou/calendarview/WeekActivity.java
@@ -0,0 +1,26 @@
+package com.example.mynameisyou.calendarview;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
+
+/**
+ * Created by MyNameIsYou on 10/21/17.
+ */
+
+public class WeekActivity extends AppCompatActivity {
+
+    private static final String TAG = "WeekActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weekly_layout);
+
+            }
+        };
+
