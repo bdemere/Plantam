bd25e804e2f91facea68d360081d9a3e2c021c6a
bdemere <bubujay7@gmail.com>
2017-11-09 14:10:52 -0500

    origin/firebase Completed pulling data from the firebase database and displaying it on the app

--- code/CalendarView/app/src/main/java/com/cpsc310proj/babib/plantam/DailyEventList.java
+++ code/CalendarView/app/src/main/java/com/cpsc310proj/babib/plantam/DailyEventList.java
@@ -1,5 +1,4 @@
 package com.cpsc310proj.babib.plantam;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -8,24 +7,33 @@ import java.util.ArrayList;
 
 public class DailyEventList {
 
-    private ArrayList<CalEvent> list;
+    public static ArrayList<Event> list = null;
 
-    //Constructor
-    public DailyEventList() {
-        ArrayList<CalEvent> event_list = new ArrayList<CalEvent>();
+//    //Constructor
+//    public DailyEventList() {
+//        ArrayList<Event> event_list = new ArrayList<Event>();
+//    }
+
+
+    public static ArrayList<Event> getEventListInstance(){
+        if(list == null){
+            list = new ArrayList<>();
+        }
+        return list;
     }
-
-
     /*method to add a new event into correct DailyEventList and sort events every time
     * 2 ways: 1.click on date and create event for the specific date
     *         2.create an event without specifying the date at first, use event handler to add
     *           event to the list
     */
-    public void addEventToList(CalEvent newEvent) {
-        ;
+    public static void addEventToList(Event newEvent) {
+        if(list == null)
+            getEventListInstance().add(newEvent);
+        else
+            list.add(newEvent);
     }
 
-    public void sortEvent(CalEvent newEvent) {
+    public void sortEvent(Event newEvent) {
         for (int i = 0; i < list.size(); i++) {
             if (newEvent.getStartTime() > list.get(i).getStartTime()){
                 list.add(i + 1, newEvent);
--- code/CalendarView/app/src/main/java/com/cpsc310proj/babib/plantam/MainActivity.java
+++ code/CalendarView/app/src/main/java/com/cpsc310proj/babib/plantam/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.cpsc310proj.babib.plantam.Layouts.CalendarActivity;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
